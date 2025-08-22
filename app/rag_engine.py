2cd3fedfa91a9969ddd18095a641822458057999
TomasWW <tomas.woelflin@gmail.com>
2025-08-22 20:33:51 +0000

    Update rag_engine

--- app/rag_engine.py
+++ app/rag_engine.py
@@ -1,15 +1,22 @@
 # rag_engine.py
-from langchain.vectorstores import Qdrant
+from langchain_community.vectorstores import Qdrant
 from qdrant_client import QdrantClient
+from langchain.embeddings.openai import OpenAIEmbeddings
 import os
 
 DB_URL = os.environ.get("DB_URL")
 RAG_ENGINE_API_KEY = os.environ.get("RAG_ENGINE_API_KEY")
 COLLECTION_NAME = "mundial_clubes_2025"
 
+# Cliente Qdrant
 qdrant_client = QdrantClient(url=DB_URL, api_key=RAG_ENGINE_API_KEY)
 
+# Inicializamos embeddings
+embeddings = OpenAIEmbeddings()  # o la clase de embeddings que uses
+
+# Vector store
 qdrant = Qdrant(
     client=qdrant_client,
-    collection_name=COLLECTION_NAME
+    collection_name=COLLECTION_NAME,
+    embeddings=embeddings
 )
